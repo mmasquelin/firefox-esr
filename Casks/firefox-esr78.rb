b5f6fc033b2b4e809b8c5a0b21e799e17c23e648
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2020-07-16 10:18:39 +0200

    Fixed multiple errors in Cask

--- Casks/firefox-esr78.rb
+++ Casks/firefox-esr78.rb
@@ -1,12 +1,11 @@
 cask 'firefox-esr78' do
     version '78.0.1'
-    sha256 'f0d9426b8060e5b72b5952e5fda6d2e6d610d5e9597e4f18dbb9eb562fa81f0b'
+    sha256 '4e39f7b7ccf221f0ffbc06849abd8b328a170cc7a8bf7c456f823c7095b41dde'
     
     # mozilla.net was verified as official when first introduced to the cask
     url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/fr/Firefox%20#{version}esr.dmg"
-    name 'Mozilla Firefox ESR'
     name 'Mozilla Firefox Extended Support Release'
-    homepage 'https://www.mozilla.org/fr-FR/firefox/organizations/'
+    homepage 'https://www.mozilla.org/en-US/firefox/enterprise/'
   
     license :mpl
   
