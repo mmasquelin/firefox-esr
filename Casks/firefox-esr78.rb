896cdaa1a2295a06b34c31f5f0110eb8268e8192
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2021-08-30 14:48:59 +0200

    Updated to v78.13.0

--- Casks/firefox-esr78.rb
+++ Casks/firefox-esr78.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr78' do
-    version '78.0.2'
-    sha256 '06c421eac5f6261d26b87b9a4a7ecc66bfc15a3c845efa765d61907dfd0df853'
+    version '78.13.0'
+    sha256 'a22ae18c7055964d8504a0444fbbe7ac36786887e3e3eae899ef83299a416ed1'
     
     # mozilla.net was verified as official when first introduced to the cask
     url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/fr/Firefox%20#{version}esr.dmg"
